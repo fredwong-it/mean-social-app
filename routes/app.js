bc980a8e70b6f1b8f6a8c599cb6f0229b231337e
Fred Wong <fredwong.it@gmail.com>
2018-03-14 23:26:30 -0400

    feat: NodeJS Basics

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,15 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message/:msg', (req, res, next) => {
+    res.render('node', {
+        message: req.params.msg
+    });
+});
+
+router.post('/message', (req, res, next) => {
+    const message = req.body.message;
+    res.redirect('/message/' + message);
+});
+
 module.exports = router;
