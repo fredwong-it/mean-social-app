63cf07e1b7c3f13f1c3e555021178c9da99bc129
Fred Wong <fredwong.it@gmail.com>
2018-03-25 16:01:05 -0400

    feat: Backend - Updating Messages

--- routes/messages.js
+++ routes/messages.js
@@ -39,4 +39,37 @@ router.post('/', function (req, res, next) {
     });
 });
 
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if (err) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: err
+            });
+        }
+
+        if (message) {
+            return res.status(500).json({
+                title: 'An error occurred',
+                error: { message: 'MEssage not found' }
+            });
+        }
+
+        message.content = req.body.content;
+        message.save(function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+    
+            res.status(200).json({
+                message: 'Updated message',
+                obj: result
+            });
+        });
+    });
+});
+
 module.exports = router;
