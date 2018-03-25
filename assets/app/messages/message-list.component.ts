62cbadb4a41c6ccc34f1a74efed6ed3e92c8d1f8
Fred Wong <fredwong.it@gmail.com>
2018-03-25 15:10:51 -0400

    feat: Getting Messages features

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -17,6 +17,12 @@ export class MessageListComponent implements OnInit {
     constructor(private messageService: MessageService) {}
 
     ngOnInit() {
-        this.messages = this.messageService.getMessages();
+        this.messageService.getMessages()
+            .subscribe(
+                (messages: Message[]) => {
+                    this.messages = messages;
+                },
+                error => console.error(error)                
+            );
     }
 }
--- routes/messages.js
+++ routes/messages.js
@@ -2,6 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Message = require('../models/message');
 
+router.get('/', function (req, res, next) {
+    Message.find()
+        .exec(function (err, messages) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'An error occurred',
+                    error: err
+                });
+            }
+
+            res.status(200).json({
+                message: 'Success',
+                obj: messages
+            });
+        });
+});
+
 router.post('/', function (req, res, next) {
     const message = new Message({
         content: req.body.content
