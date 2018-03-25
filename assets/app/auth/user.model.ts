0726151fa4919ff7810775b9a83b47cc3ed1980d
Fred Wong <fredwong.it@gmail.com>
2018-03-25 18:15:27 -0400

    fix: change the name and clean up the front end User model

--- assets/app/auth/user.model.ts
+++ assets/app/auth/user.model.ts
@@ -1,9 +1,4 @@
-export class Message {
-    content: string;
-    username: string;
-    messageId: string;
-    userId: string;
-
+export class User {
     constructor(public email: string,
                 public password: string,
                 public firstName?: string,
