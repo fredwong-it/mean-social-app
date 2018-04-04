b76ac9903a7fa22af73d3b0a6073a6bd38d9742d
Fred Wong <fredwong.it@gmail.com>
2018-04-03 23:37:17 -0400

    feat: creating the MessageModule

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,16 +1,13 @@
+import { MessageModule } from './messages/message.module';
 import { ErrorService } from './errors/error.service';
 import { AuthService } from './auth/auth.service';
 import { SignupComponent } from './auth/signup.component';
 import { routing } from './app.routing';
 import { HeaderComponent } from './header.component';
-import { MessageInputComponent } from './messages/message-input.component';
-import { MessageListComponent } from './messages/message-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
-import { MessageComponent } from './messages/message.component';
-import { MessagesComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { LogoutComponent } from './auth/logout.component';
 import { SigninComponent } from './auth/signin.component';
@@ -20,10 +17,6 @@ import { ErrorComponent } from './errors/error.component';
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent,
-        MessageListComponent,
-        MessageInputComponent,
-        MessagesComponent,
         AuthenticationComponent,
         HeaderComponent,
         LogoutComponent,
@@ -33,10 +26,10 @@ import { ErrorComponent } from './errors/error.component';
     ],
     imports: [
         BrowserModule,
-        FormsModule,
         routing,
         ReactiveFormsModule,
-        HttpModule
+        HttpModule,
+        MessageModule
     ],
     providers: [
         AuthService,
